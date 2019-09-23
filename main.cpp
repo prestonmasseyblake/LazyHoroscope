a1eef78c65f82a80064284ee75028412fc262be7
prestonmasseyblake <pmassey-blake@mail.csuchico.edu>
2019-09-23 16:47:49 -0700

    LuckyNumberNameGenerator

--- main.cpp
+++ main.cpp
@@ -9,14 +9,15 @@ int main()
   //declare variables
 string first = "";
 string last= "";
-
+int lucky = 0;
   //get user input
 cout<<"What is your first name?\n";
 cin>>first;
-cout<<"Whatis your last name?\n";
+cout<<"What is your last name?\n";
 cin>>last;
 cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"., here is your fortune...";
   //tell fortune
+cout<<"Your lucky number is "<<first.length()<<".";
 
   return 0;
 }
