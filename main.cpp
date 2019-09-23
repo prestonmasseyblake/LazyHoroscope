5e158c421e20c7edb5c3f3f4a0381a00f4287b8c
prestonmasseyblake <pmassey-blake@mail.csuchico.edu>
2019-09-23 16:32:53 -0700

    FirstLastHoriscope

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Preston Massey-Blake
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
+string first = "";
+string last= "";
 
   //get user input
-
+cout<<"What is your first name?\n";
+cin>>first;
+cout<<"Whatis your last name?\n";
+cin>>last;
+cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"., here is your fortune...";
   //tell fortune
 
   return 0;
