37dc3c4f74b58c6beb9eb80036384702231fe846
prestonmasseyblake <pmassey-blake@mail.csuchico.edu>
2019-09-25 19:41:05 -0700

    LazyHoriscopeFortuneTeller

--- main.cpp
+++ main.cpp
@@ -15,9 +15,41 @@ cout<<"What is your first name?\n";
 cin>>first;
 cout<<"What is your last name?\n";
 cin>>last;
-cout<<"Welcome,"<<first[0]<<"."<<last[0]<<"., here is your fortune...";
+cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune...\n";
   //tell fortune
-cout<<"Your lucky number is "<<first.length()<<".";
-
+cout<<"your lucky number is "<<first.length()<<endl;
+if (first[0]== 'a') { 
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'A') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'e') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'E') {
+cout<<"you are destined to be ramous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'i') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'I') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'o') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'O') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'u') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else if (first[0]== 'U') {
+cout<<"you are destined to be famous!\nyou have already met your true love.\nhave a good day!"<<endl;
+}
+else  {
+cout<<"you should keep a low profile.\nhave a good day!"<<endl;
+}
   return 0;
 }
